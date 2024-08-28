0e9fe25ef5088ce85338061162cca129e90ebfa5
Alexander <allanosinhb@gmail.com>
2024-08-28 16:43:25 -0700

    added bears schedule to seeder files

--- backend/db/seeders/20240827015551-questions.js
+++ backend/db/seeders/20240827015551-questions.js
@@ -21,6 +21,74 @@ module.exports = {
       {
         type: "over/under",
         content: "Chargers 17.5 week 6"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Titans week 1"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Texans week 2"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Colts week 3"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Rams week 4"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Panthers week 5"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Jaguars week 6 "
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Commanders week 8"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Cardinals week 9"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Patriots week 10"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Packers week 11"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Vikings week 12"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Lions week 13"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears 49ers week 14"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Vikings week 15"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Lions week 16"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Seahawks week 17"
+      },
+      {
+        type: "Win or lose",
+        content: "Bears Packers week 18"
       }
     ])
     /**
