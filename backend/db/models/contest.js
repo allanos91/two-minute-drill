b6af686fdae099e22d3cbef16c1d6263d34ca633
Alexander <allanosinhb@gmail.com>
2024-08-29 18:44:38 -0700

    test6

--- backend/db/models/contest.js
+++ backend/db/models/contest.js
@@ -11,12 +11,12 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Contest.belongsTo(
-        models.User,
-        {
-          foreignKey: 'host_id',
-        }
-      )
+      // Contest.belongsTo(
+      //   models.User,
+      //   {
+      //     foreignKey: 'host_id',
+      //   }
+      // )
       Contest.belongsToMany(
         models.Prediction,
         {
